aebc413a9f8939b1c8b7fd40227f39c63a60f7d2
Kevin <Kevinorigami2013@gmail.com>
2020-03-06 22:09:21 -0300

    Tela emergencia

---
+++ app/src/main/java/br/com/etecia/bullseye/Emergencia.java
@@ -0,0 +1,14 @@
+package br.com.etecia.bullseye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Emergencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergencia);
+    }
+}
