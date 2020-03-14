52fcb1748ce3832d1880d1517529fee39fdc6d26
JeffersonSoares99 <jefferson99soares@gmail.com>
2020-03-13 22:12:04 -0300

    Tela emergencia melhorada

---
+++ app/src/main/java/br/com/etecia/bullseye/Avaliacao.java
@@ -0,0 +1,14 @@
+package br.com.etecia.bullseye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Avaliacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avaliacao_layout);
+    }
+}
